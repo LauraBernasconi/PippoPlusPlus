e850f91627ce9eca712111d17a8299a12db29fdd
Laura Bernasconi <me@laurabernasconi.com>
2025-01-13 23:06:38 +0100

    First version successfully created

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # PippoPlusPlus
+
+Hello World by Nanowar of Steel written in Python. I love PippoPlusPlus :)
+
+Work in progress ;)
+
---
+++ main.py
@@ -0,0 +1,13 @@
+def HelloWorld(foo):
+    pippo = 0
+    while (pippo < foo):
+        print("HelloWorld")
+        pippo += 1
+
+def main():
+    HelloWorld(2)
+
+if __name__ == "__main__":
+    main()
+
+
